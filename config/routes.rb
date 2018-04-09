fd367ebed794246c364063fb15f188221ad01e0d
capaisa12 <capaisa12@gmail.com>
2018-04-08 19:49:55 -0500

    Se crea el metodo show de questions

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 get '/questions', to: 'questions#index'
 get '/questions/new', to: 'questions#new', as: 'new_questions'
 post '/questions', to: 'questions#create'
+get '/questions/:id', to: 'questions#show'
 
 end
